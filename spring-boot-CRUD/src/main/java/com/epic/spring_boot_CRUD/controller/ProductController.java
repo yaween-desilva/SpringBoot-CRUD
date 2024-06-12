7cd2c1a09645c38ce6b48ce222e64ed0dc93f64b
Yaween De Silva <yaween2003@gmail.com>
2024-06-12 12:55:10 +0530

    ProductController class was added to the controller package which consists the API endpoints and execution of CRUD operations

---
+++ spring-boot-CRUD/src/main/java/com/epic/spring_boot_CRUD/controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.epic.spring_boot_CRUD.controller;
+
+import com.epic.spring_boot_CRUD.dto.ProductDTO;
+import com.epic.spring_boot_CRUD.entity.Product;
+import com.epic.spring_boot_CRUD.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @PostMapping("/add")
+    public ProductDTO createProduct(@RequestBody ProductDTO productDTO){
+        return productService.createProduct(productDTO);
+    }
+
+    @GetMapping("/getAll")
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/getAll/{id}")
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id){
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetails){
+        Product updatedProduct = productService.updateProduct(id, productDetails);
+        return ResponseEntity.ok(updatedProduct);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+    }
+}
