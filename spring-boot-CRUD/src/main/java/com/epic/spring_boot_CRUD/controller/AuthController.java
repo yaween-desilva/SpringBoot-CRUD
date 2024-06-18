6dec38dfc57fce3df6415bfc7c34d43ddebf9413
Yaween De Silva <yaween2003@gmail.com>
2024-06-18 11:03:58 +0530

    Authcontroller java file was added

---
+++ spring-boot-CRUD/src/main/java/com/epic/spring_boot_CRUD/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.epic.spring_boot_CRUD.controller;
+
+import com.epic.spring_boot_CRUD.service.TokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final TokenService tokenService;
+
+    public AuthController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        return tokenService.generateToken(authentication);
+    }
+
+}
