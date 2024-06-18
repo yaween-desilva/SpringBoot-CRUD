05d106867734254ea846b92ca7124452fc0f934c
Yaween De Silva <yaween2003@gmail.com>
2024-06-18 11:02:38 +0530

    UserController java file was added

---
+++ spring-boot-CRUD/src/main/java/com/epic/spring_boot_CRUD/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.epic.spring_boot_CRUD.controller;
+
+import com.epic.spring_boot_CRUD.entity.User;
+import com.epic.spring_boot_CRUD.service.UserService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user){
+        if(userService.findByUsername(user.getUsername()) != null){
+            return ResponseEntity.badRequest().body("Username is not found");
+        }
+        userService.registerUser(user);
+        return ResponseEntity.ok("User Registered Successfully");
+    }
+}
