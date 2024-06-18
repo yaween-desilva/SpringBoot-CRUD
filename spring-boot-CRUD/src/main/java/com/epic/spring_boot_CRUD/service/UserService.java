d39396adbcdd0596268922e47a4e1553aae0bc66
Yaween De Silva <yaween2003@gmail.com>
2024-06-18 11:00:54 +0530

    UserService java file was added

---
+++ spring-boot-CRUD/src/main/java/com/epic/spring_boot_CRUD/service/UserService.java
@@ -0,0 +1,26 @@
+package com.epic.spring_boot_CRUD.service;
+
+import com.epic.spring_boot_CRUD.entity.User;
+import com.epic.spring_boot_CRUD.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+}
